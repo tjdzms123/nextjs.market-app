b53d1b63e44b3e9742d54b0919c87bc790720bb3
David Kim <tjdzms123@naver.com>
2024-02-04 00:59:10 +0900

    first commit

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,34 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
+import React from 'react';
+
+const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
+
+  return (
+    <ul
+      className={`text-md justify-content flex gap-4 w-full items-center ${
+        mobile && 'flex-col h-full'
+      }`}
+    >
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <Link href="/admin">Admin</Link>
+      </li>
+      <li className="py-2 text-center border-b-4 cursor-pointer">
+        <Link href="/user">User</Link>
+      </li>
+      {session?.user ? (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={()=>signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className="py-2 text-center border-b-4 cursor-pointer">
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default NavItem;
