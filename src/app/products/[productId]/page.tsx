bb561be4a00d00a990c63ab64085bb9583f54e80
David Kim <tjdzms123@naver.com>
2024-02-29 02:29:07 +0900

    상세페이지 구현 완료 채팅 기능 시작

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,27 @@
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import getProductById from '@/app/actions/getProductById';
+import EmptyState from '@/components/EmptyState';
 import React from 'react';
+import ProductClient from './ProductClient';
 
-const ProductPage = () => {
-  return <div>ProductPage</div>;
+interface Params {
+  productId?: string;
+}
+
+const ProductPage = async ({ params }: { params: Params }) => {
+  const product = await getProductById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!product) {
+    return <EmptyState />;
+  }
+
+  return (
+    <ProductClient
+      product={product}
+      currentUser={currentUser}
+    />
+  );
 };
 
 export default ProductPage;
